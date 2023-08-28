abd39e1eba765599e560e8763ae0c8e2f37e2e1e
grmartins <grmartins@ciasc.sc.gov.br>
2023-08-28 11:00:49 -0300

    Ajustes na estrutura dos dados.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,38 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 export class Pessoa {
   nome!: string;
-  sobrenome!: string;
-  endereco!: string;
+  cpf!: string;
+  idade!: number;
+  campo1!: string;
+  campo2!: string;
+  campo3!: string;
+  endereco = new Endereco();
 
   constructor(json?: any) {
     if (json) {
       this.nome = json.nome;
-      this.sobrenome = json.sobrenome;
-      this.endereco = json.endereco;
+      this.cpf = json.cpf;
+      this.endereco = new Endereco(json.endereco);
+      this.idade = json.idade;
+      this.campo1 = json.campo1;
+      this.campo2 = json.campo2;
+      this.campo3 = json.campo3;
+    }
+  }
+}
+
+export class Endereco {
+  logradouro!: string;
+  bairro!: string;
+  complemento!: string;
+
+  constructor(json?: any) {
+    if (json) {
+      this.logradouro = json.logradouro;
+      this.bairro = json.bairro;
+      this.complemento = json.complemento;
     }
   }
 }
@@ -24,7 +47,10 @@ export class AppComponent implements OnInit{
   title = 'exemplo2';
 
   pessoa!: Pessoa;
-  umaPessoa: Pessoa = new Pessoa({nome: 'Gilberto', sobrenome: 'Rodrigues', endereco: 'Thiago da Fonseca'});
+  umaPessoa: Pessoa = new Pessoa({nome: 'Gilberto', cpf: '12345678901', idade: 50, endereco: new Endereco({logradouro: 'Thiago da Fonseca'})});
+
+  constructor(private httpClient: HttpClient) {
+  }
 
   ngOnInit(): void {
     this.pessoa = new Pessoa();
@@ -33,13 +59,18 @@ export class AppComponent implements OnInit{
 
   enviar(): void {
     console.log('Vai enviar os dados da pessoa para o backend: ' + JSON.stringify(this.pessoa));
+    this.httpClient.post('http://localhost:8080/pessoas', this.pessoa).subscribe({
+      next: () => console.log(''),
+      error: (erros: any) => console.log(JSON.stringify(erros.error))
+    })
   }
 
   print(): string {
     return `{
       nome: string
-      sobrenome: string
-      endereco: string
+      cpf: string
+      idade: number
+      endereco: Endereco
     }`;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
+    HttpClientModule,
     BrowserModule,
     FormsModule,
     AppRoutingModule
